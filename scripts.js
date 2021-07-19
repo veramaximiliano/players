561959869fc39ad05a46b4260325be845f6954ce
Maximiliano Vera <maximilianovera@Maximilianos-MacBook-Pro.local>
2021-07-19 00:45:10 -0300

    update video player

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const player = new Plyr('#player', {
-    fullscreen: { enabled: true, fallback: true, iosNative: true }
+    fullscreen: { enabled: true, fallback: 'force', iosNative: false }
   });
 })
 
